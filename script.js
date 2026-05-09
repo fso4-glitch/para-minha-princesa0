ba004ab27056708e7ab401a3d7d548e06567b295
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-09 17:39:32 +0000

    fix: puzzle funcionando

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
 const puzzle = document.getElementById("puzzle");
 
+// posições corretas
 let positions = [
-    "0px 0px",
-    "-120px 0px",
-    "-240px 0px",
-    "0px -120px",
-    "-120px -120px",
-    "-240px -120px",
-    "0px -240px",
-    "-120px -240px",
-    "-240px -240px"
+  "0px 0px",
+  "-120px 0px",
+  "-240px 0px",
+  "0px -120px",
+  "-120px -120px",
+  "-240px -120px",
+  "0px -240px",
+  "-120px -240px",
+  "-240px -240px"
 ];
 
-// embaralhar
-positions.sort(() => Math.random() - 0.5);
+// EMBARALHAR FORTE (garantido)
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
-// criar peças
+shuffle(positions);
+
+// LIMPA antes (IMPORTANTE)
+puzzle.innerHTML = "";
+
+// cria peças
 positions.forEach(pos => {
-    let div = document.createElement("div");
-    div.className = "piece";
-    div.style.backgroundPosition = pos;
-    puzzle.appendChild(div);
+  let div = document.createElement("div");
+  div.className = "piece";
+  div.style.backgroundPosition = pos;
+  puzzle.appendChild(div);
 });
 
+// troca no toque (celular)
 let primeira = null;
 
 document.querySelectorAll(".piece").forEach(piece => {
+  piece.addEventListener("click", () => {
+    if (!primeira) {
+      primeira = piece;
+      piece.style.border = "2px solid red";
+    } else {
+      let temp = primeira.style.backgroundPosition;
+      primeira.style.backgroundPosition = piece.style.backgroundPosition;
+      piece.style.backgroundPosition = temp;
 
-    piece.addEventListener("touchstart", () => {
-        if (!primeira) {
-            primeira = piece;
-            piece.style.border = "2px solid red";
-        } else {
-            let temp = primeira.style.backgroundPosition;
-            primeira.style.backgroundPosition = piece.style.backgroundPosition;
-            piece.style.backgroundPosition = temp;
-
-            primeira.style.border = "1px solid #fff";
-            primeira = null;
-        }
-    });
-
+      primeira.style.border = "1px solid #fff";
+      primeira = null;
+    }
+  });
 });
