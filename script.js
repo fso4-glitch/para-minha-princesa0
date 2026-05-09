2c240981a7bb26690a1d10ada9f0fe6e437dc880
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-09 17:47:08 +0000

    puzzle funcionando

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
 const puzzle = document.getElementById("puzzle");
 
-// posições das peças (0 a 8)
-let pieces = [0,1,2,3,4,5,6,7,8];
+// posições da imagem
+let positions = [
+  "0px 0px",
+  "-120px 0px",
+  "-240px 0px",
+  "0px -120px",
+  "-120px -120px",
+  "-240px -120px",
+  "0px -240px",
+  "-120px -240px",
+  "-240px -240px"
+];
 
-// embaralhar de verdade
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-shuffle(pieces);
-
-// limpa antes
-puzzle.innerHTML = "";
-
-// cria peças embaralhadas
-pieces.forEach(num => {
-    let div = document.createElement("div");
-    div.className = "piece";
-
-    div.style.backgroundImage = "url('foto.jpg')";
-
-    let x = (num % 3) * 120;
-    let y = Math.floor(num / 3) * 120;
-
-    div.style.backgroundPosition = `-${x}px -${y}px`;
-
-    puzzle.appendChild(div);
+// criar peças
+positions.forEach(pos => {
+  let div = document.createElement("div");
+  div.className = "piece";
+  div.style.backgroundPosition = pos;
+  puzzle.appendChild(div);
 });
 
-// troca ao tocar/clicar
+// função embaralhar
+function embaralhar() {
+  let pieces = document.querySelectorAll(".piece");
+  let posicoes = [];
+
+  pieces.forEach(p => posicoes.push(p.style.backgroundPosition));
+
+  for (let i = posicoes.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [posicoes[i], posicoes[j]] = [posicoes[j], posicoes[i]];
+  }
+
+  pieces.forEach((p, i) => {
+    p.style.backgroundPosition = posicoes[i];
+  });
+}
+
+// trocar peças
 let primeira = null;
 
 document.querySelectorAll(".piece").forEach(piece => {
-    piece.addEventListener("click", () => {
-        if (!primeira) {
-            primeira = piece;
-            piece.style.border = "2px solid red";
-        } else {
-            let temp = primeira.style.backgroundPosition;
-            primeira.style.backgroundPosition = piece.style.backgroundPosition;
-            piece.style.backgroundPosition = temp;
+  piece.addEventListener("click", () => {
+    if (!primeira) {
+      primeira = piece;
+      piece.style.border = "2px solid red";
+    } else {
+      let temp = primeira.style.backgroundPosition;
+      primeira.style.backgroundPosition = piece.style.backgroundPosition;
+      piece.style.backgroundPosition = temp;
 
-            primeira.style.border = "1px solid #fff";
-            primeira = null;
-        }
-    });
+      primeira.style.border = "1px solid #fff";
+      primeira = null;
+    }
+  });
 });
